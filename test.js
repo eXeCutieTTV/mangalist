573ac66a61b9b0e7d2c09de70debea9048d3dfa8
eXeCutieTTV <152514196+eXeCutieTTV@users.noreply.github.com>
2026-01-05 22:41:40 +0100

    a

--- test.js
+++ test.js
@@ -1,14 +1,15 @@
-const { Mangadex } = import("mangadex-full-api");
+const { MangadexClient } = import("mangadex-full-api");
 
 (async () => {
-  const client = new Mangadex();
+    // create a client
+    const client = new MangadexClient();
 
-  // search for One Piece
-  const mangaResults = await client.manga.search({ title: "One Piece", limit: 5 });
-  const onePiece = mangaResults[0]; // pick the main Japanese one
+    // search for One Piece
+    const mangaResults = await client.manga.search({ title: "One Piece", limit: 5 });
+    const onePiece = mangaResults[0]; // pick the main Japanese one
 
-  // fetch chapters (JP)
-  const chapters = await onePiece.getChapters({ translatedLanguage: ["ja"] });
+    // fetch chapters (JP)
+    const chapters = await onePiece.getChapters({ translatedLanguage: ["ja"] });
 
-  console.log(chapters);
+    console.log(chapters);
 })();
