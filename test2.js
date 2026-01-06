d8d40946132cdf58ecc46089e9222dc7c9f91428
eXeCutie <danielgehlert2007@gmailcom>
2026-01-06 19:44:27 +0100

    x

--- test2.js
+++ test2.js
@@ -2,7 +2,7 @@
 import fetch from "node-fetch";
 
 export default async function handler(req, res) {
-    if (req.method !== "POST") return res.status(405).send("Method Not Allowed");
+    //if (req.method !== "POST") return res.status(405).send("Method Not Allowed");
 
     const { filename, content } = req.body;
 
@@ -10,6 +10,7 @@ export default async function handler(req, res) {
     const repoName = "mangalist";
     const workflowId = "upload-generated-file.yml";
     const token = process.env.GH_TOKEN; // stored safely on server
+    console.log(token);
 
     const response = await fetch(
         `https://api.github.com/repos/${repoOwner}/${repoName}/actions/workflows/${workflowId}/dispatches`,
