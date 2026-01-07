7a657d6bf1db1ee14c89a0b5fe45c9662c5a7406
eXeCutieTTV <danielgehlert2007@gmail.com>
2026-01-07 10:26:25 +0100

    x

--- pages/page2/page2.js
+++ pages/page2/page2.js
@@ -949,7 +949,7 @@ function page2() {
             const timestamp = Date.now();
             const filename = `generated_${timestamp}.js`;
             const content = `
-                const entryMap = ${JSON.stringify(entryMap, null, 2)};
+                const entryMap = ${"JSON.stringify(entryMap, null, 2)"};
                 const div = document.createElement("div");
                 div.id = "page2-wait-for-element";
                 document.body.appendChild(div);
