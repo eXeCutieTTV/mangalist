385e30c4ee46325d7c7f45d27a48ec3e0a07cb83
eXeCutie <danielgehlert2007@gmailcom>
2026-01-11 14:45:19 +0100

    bleach volume obj works

--- fandom_scrapers-_eXeCutie/bleach/bleach.js
+++ fandom_scrapers-_eXeCutie/bleach/bleach.js
@@ -1,15 +1,20 @@
+//0indexed
+const temp = [];
 function bleach(id) {
     const wrapper = document.getElementsByClassName('mw-content-ltr')[0];
-    const volume_section = Math.floor(id / 10);
-    const tbl = wrapper.querySelectorAll('table');
-    const trs = tbl[volume_section].querySelectorAll('tr');
-    //for(let i=0;i<74;i++) console.log(Math.floor(i/10));
-    //console.log(trs);
-    const volume1 = {
-        header: trs[id * volume_section + 2],
-        body: trs[id * volume_section + 3],
-        desc: trs[id * volume_section + 4]
-    }
-    return volume1;
+    const tables = wrapper.querySelectorAll('table');
+
+    const tableIndex = Math.floor(id / 10);
+    const rowIndex = 2 + (id % 10) * 3;
+
+    const trs = tables[tableIndex].querySelectorAll('tr');
+
+    const volume = {
+        header: trs[rowIndex],
+        body: trs[rowIndex + 1],
+        desc: trs[rowIndex + 2]
+    };
+
+    temp.push(volume);
 }
-for (let i = 0; i < 74; i++) bleach(i); 
+for (let i = 0; i < 74; i++) bleach(i);
