9d36c94710ecfb9e04779a54db9356cc9e731f63
eXeCutie <danielgehlert2007@gmailcom>
2026-02-09 16:08:37 +0100

    temp

---
+++ fandom_scrapers/jojos/jojos.js
@@ -0,0 +1,85 @@
+const temp = {};
+const imgs = [];
+function jojos(id) {
+    /*
+    const index_id = id <= 3
+        ? `PB_Volume_${id}`
+        : id <= 7
+            ? `BT_Volume_${id - 3}`
+            : id <= 17
+                ? `SC_Volume_${id - 7}`
+                : id <= 26
+                    ? `DU_Volume_${id - 17}`
+                    : id <= 35
+                        ? `Volume_${id - 26}`
+                        : id <= 44
+                            ? `Volume_${id - 35}`
+                            : id <= 52
+                                ? `Volume_${id - 44}`
+                                : null;
+                                */
+    let index_id = null;
+    let url = null;
+    if (id <= 3) {
+        index_id = `PB_Volume_${id + 1}`;
+        url = `https://jojowiki.com/List_of_English_JoJo%27s_Bizarre_Adventure_Chapters/Part_1`;
+    } else if (id <= 7) {
+        index_id = `BT_Volume_${id - 3 + 1}`;
+        url = `https://jojowiki.com/List_of_English_JoJo%27s_Bizarre_Adventure_Chapters/Part_2`;
+    } else if (id <= 17) {
+        index_id = `SC_Volume_${id - 7 + 1}`;
+        url = `https://jojowiki.com/List_of_English_JoJo%27s_Bizarre_Adventure_Chapters/Part_3_Hardcover`;
+    } else if (id <= 26) {
+        index_id = `DU_Volume_${id - 17 + 1}`;
+        url = `https://jojowiki.com/List_of_English_JoJo%27s_Bizarre_Adventure_Chapters/Part_4`;
+    } else if (id <= 35) {
+        index_id = `Volume_${id - 26 + 1}`;
+        url = `https://jojowiki.com/List_of_English_JoJo%27s_Bizarre_Adventure_Chapters/Part_5`;
+    } else if (id <= 44) {
+        index_id = `Volume_${id - 35 + 1}`;
+        url = `https://jojowiki.com/List_of_English_JoJo%27s_Bizarre_Adventure_Chapters/Part_6`;
+    } else if (id <= 52) {
+        index_id = `Volume_${id - 44 + 1}`;
+        url = `https://jojowiki.com/List_of_English_JoJo%27s_Bizarre_Adventure_Chapters/Part_7`;
+    }
+    const tbl = document.getElementById(index_id)
+    const tblObj = {
+        header: tbl.children[0].children,
+        body: tbl.children
+    }
+    //console.log(tblObj);
+    const bodyObj = {
+        header: tblObj.body[0],
+        info: tblObj.body[1]
+    }
+    //console.log(bodyObj)
+    const infoObj = {
+        img: bodyObj.info.children[0],
+        chapters: bodyObj.info.querySelector(".volumeTableChList").querySelectorAll("li")
+    }
+    //console.log(infoObj);
+
+
+    const result = {
+        title: [bodyObj.header.querySelector(".volumeTableTitle").innerText.replace(/\n/, "").trim(), bodyObj.header.querySelector(".volumeTableTitle").innerText.replace(/\n/, "").trim()],
+        date_release: [bodyObj.info.querySelector(".volumeTableInfo").children[1].children[0].innerText.replace(/\[\d+\]/, "").trim(), bodyObj.info.querySelector(".volumeTableInfo").children[1].children[0].innerText.replace(/\[\d+\]/, "").trim()],
+        pages: [],
+        ISBN: [bodyObj.info.querySelector(".volumeTableInfo").children[1].children[1].innerText.trim(), bodyObj.info.querySelector(".volumeTableInfo").children[1].children[1].innerText.trim()],
+        chapters: {},
+        cover_characters: [],
+        img: `images/jojos/vol_${String(id + 1).padStart(3, "0")}.png`,
+        owned: false,
+        prise: "10 €",
+        date_bought: ""
+    }
+    for (const chapter of infoObj.chapters) {
+        const titles = [chapter.children[0].children[0], chapter.children[0].children[1].children[1].innerHTML.replace(/<ruby[^>]*>.*?<rb>(.*?)<\/rb>.*?<\/ruby>/g, "$1").replace(/<[^>]+>/g, "")];
+        //^^ removes furigana
+        let number = $(chapter).index() + chapter.parentElement.start;
+        result.chapters[number] = [titles[0], titles[1]];
+    }
+    temp[id + 1] = result;
+    imgs.push(infoObj.img.querySelector("img").src);
+}
+jojos(2)
+console.log(temp)
---
+++ fandom_scrapers/jojos/test.js
@@ -0,0 +1,111 @@
+import fetch from "node-fetch";
+import * as cheerio from "cheerio";
+import { writeFile } from "fs/promises";
+import fs from "fs";
+
+const temp = {};
+const imgs = [];
+
+function getVolumeInfo(id) {
+    if (id <= 3) {
+        return {
+            index_id: `PB_Volume_${id + 1}`,
+            url: `https://jojowiki.com/List_of_English_JoJo%27s_Bizarre_Adventure_Chapters/Part_1`
+        };
+    } else if (id <= 7) {
+        return {
+            index_id: `BT_Volume_${id - 3 + 1}`,
+            url: `https://jojowiki.com/List_of_English_JoJo%27s_Bizarre_Adventure_Chapters/Part_2`
+        };
+    } else if (id <= 17) {
+        return {
+            index_id: `SC_Volume_${id - 7 + 1}`,
+            url: `https://jojowiki.com/List_of_English_JoJo%27s_Bizarre_Adventure_Chapters/Part_3_Hardcover`
+        };
+    } else if (id <= 26) {
+        return {
+            index_id: `DU_Volume_${id - 17 + 1}`,
+            url: `https://jojowiki.com/List_of_English_JoJo%27s_Bizarre_Adventure_Chapters/Part_4`
+        };
+    } else if (id <= 35) {
+        return {
+            index_id: `Volume_${id - 26 + 1}`,
+            url: `https://jojowiki.com/List_of_English_JoJo%27s_Bizarre_Adventure_Chapters/Part_5`
+        };
+    } else if (id <= 44) {
+        return {
+            index_id: `Volume_${id - 35 + 1}`,
+            url: `https://jojowiki.com/List_of_English_JoJo%27s_Bizarre_Adventure_Chapters/Part_6`
+        };
+    } else if (id <= 52) {
+        return {
+            index_id: `Volume_${id - 44 + 1}`,
+            url: `https://jojowiki.com/List_of_English_JoJo%27s_Bizarre_Adventure_Chapters/Part_7`
+        };
+    }
+    return null;
+}
+
+async function jojos(id) {
+    const info = getVolumeInfo(id);
+    if (!info) return;
+
+    const res = await fetch(info.url);
+    const html = await res.text();
+    const $ = cheerio.load(html);
+
+    const tbl = $(`#${info.index_id}`);
+
+    const header = tbl.children().eq(0);
+    const body = tbl.children().eq(1);
+
+    const titleText = header.find(".volumeTableTitle").text().replace(/\n/, "").trim();
+
+    const infoBox = body.find(".volumeTableInfo");
+    const release = infoBox.children().eq(1).children().eq(0).text().replace(/\[\d+\]/, "").trim();
+    const isbn = infoBox.children().eq(1).children().eq(1).text().trim();
+
+    const chapterList = body.find(".volumeTableChList li");
+
+    const chapters = {};
+
+    const ol = body.find(".volumeTableChList").parent("ol");
+    const start = Number(ol.attr("start") || 1);
+
+    chapterList.each((i, el) => {
+        const li = $(el);
+
+        const jp = li.find("a").eq(0).text().trim();
+
+        const rawHtml = li.find("a").eq(1).html() || "";
+        const en = rawHtml
+            .replace(/<ruby[^>]*>.*?<rb>(.*?)<\/rb>.*?<\/ruby>/g, "$1")
+            .replace(/<[^>]+>/g, "")
+            .trim();
+
+        const number = start + i;
+
+        chapters[number] = [jp, en];
+    });
+
+    const imgUrl = body.find("img").attr("src") || "";
+
+    imgs.push(imgUrl);
+
+    temp[id + 1] = {
+        title: [titleText, titleText],
+        date_release: [release, release],
+        pages: [],
+        ISBN: [isbn, isbn],
+        chapters,
+        cover_characters: [],
+        img: `images/jojos/vol_${String(id + 1).padStart(3, "0")}.png`,
+        owned: false,
+        prise: "10 €",
+        date_bought: ""
+    };
+}
+
+// example
+await jojos(2);
+console.log(temp);
